d5b403408d1d0769e9f0aa3b1d4449d28a020f93
Daniel Bo <bodangren@gmail.com>
2026-04-19 01:22:21 +0800

    chore: remove tracked dist/node_modules from git, add package-lock.json

--- apps/integrated-math-3/dist/server/index.js
+++
@@ -1,7 +0,0 @@
-globalThis.__VINEXT_LAZY_CHUNKS__ = ["assets/router-tRr8GIuY.js","assets/__vite-browser-external-CRgqSluK.js","assets/page-nfu2wOWs.js","assets/MarkdownRenderer-DnAp9kEH.js","assets/ConvexClientProvider-C4kduzvU.js"];
-import { w } from "./assets/worker-entry-DvG8JxDD.js";
-import "node:async_hooks";
-import "./__vite_rsc_assets_manifest.js";
-export {
-  w as default
-};
